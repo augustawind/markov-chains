92a33e2f6b272b3c4f7435eea85fe014ac05d6b2
Dustin Rohde <dustin.rohde@gmail.com>
2016-11-06 13:34:34 -0500

    Second commit.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,29 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Lib where
 
-import Control.Monad (replicateM, void)
+import           Control.Monad          (replicateM, void)
 import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Control.Monad.Random   (MonadRandom, RandT, getRandom, runRandT)
-import           Control.Monad.Reader   (MonadReader, ReaderT, ask, runReaderT)
-import           Control.Monad.State    (MonadState, StateT, get, put, runStateT)
+import           Control.Monad.Random   (MonadRandom, RandT, getRandom,
+                                         runRandT)
+import           Control.Monad.Reader   (MonadReader, ReaderT, asks, runReaderT)
+import           Control.Monad.State    (MonadState, StateT, get, put,
+                                         runStateT)
 import           Data.List              (findIndex)
+import           Data.Maybe             (fromMaybe)
 import           System.Random          (StdGen, newStdGen)
 
+-- | A Probability is a number n, where 0 <= n < 1.
+type Probability = Double
+
+-- | A Markov is a matrix of probabilities.
+newtype Markov = Markov
+    { getMarkov :: [[Probability]]
+    } deriving (Show)
+
+-- | An MState (Markov state) is an index in the matrix.
+type MState = Int
+
+-- | An App is a Markov chain with a current state and a Random Gen.
 newtype App a = App
     { runApp :: ReaderT Markov (StateT MState (RandT StdGen IO)) a
     } deriving ( Functor
@@ -20,40 +35,37 @@ newtype App a = App
                , MonadIO
                )
 
-type Probability = Double
+run :: Markov -> MState -> StdGen -> App a -> IO ((a, MState), StdGen)
+run markov mstate gen app = do
+    runRandT (runStateT (runReaderT (runApp app) markov) mstate) gen
 
-data Markov = Markov [[Probability]]
-    deriving (Show)
-
-type MState = Int
-
+-- | Get the next state.
 nextState :: App MState
 nextState = do
-    Markov markov <- ask
-    index <- get
+    markov <- asks getMarkov
+    currentState <- get
     rand <- getRandom :: App Double
 
-    let row = markov !! index
-        accumProbs = scanl1 (+) row
-        nextIndex =
-            case findIndex (> rand) accumProbs of
-                Nothing -> length row - 1
-                Just i -> i
+    let
+        -- Get the current "state" in the Markov chain
+        row = markov !! currentState
+        -- Create a list of probability ranges
+        accumulatedSum = scanl1 (+) row
+        -- The random number's index is the next state
+        nextState' = fromMaybe
+            (-1)
+            (findIndex (> rand) accumulatedSum)
 
-    put nextIndex
-    return nextIndex
+    -- Update and return the new state
+    put nextState'
+    return nextState'
 
 exampleMarkov :: Markov
 exampleMarkov = Markov
-    [ [1.0, 0.0, 0.0]
-    , [1.0, 0.0, 0.0]
+    [ [0.0, 1.0, 0.0]
+    , [0.0, 0.0, 1.0]
     , [1.0, 0.0, 0.0]
     ]
-
-run :: Markov -> MState -> App a -> IO ((a, MState), StdGen)
-run markov mstate app = do
-    gen <- newStdGen
-    runRandT (runStateT (runReaderT (runApp app) markov) mstate) gen
 
 printStates :: Int -> App ()
 printStates n = void $ replicateM n (nextState >>= liftIO . print)
